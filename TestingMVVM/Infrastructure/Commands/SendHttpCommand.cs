a5f172d83906e724f485c705115d46dd6fbd81be
DESKTOP-GKEIH8H\EVGEN <evgen29031999@gmail.com>
2021-09-07 15:15:13 +0300

    отдельная команда

---
+++ TestingMVVM/Infrastructure/Commands/SendHttpCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingMVVM.Infrastructure.Commands.Base;
+
+namespace TestingMVVM.Infrastructure.Commands
+{
+    class SendHttpCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            // какое-то действие
+        }
+    }
+}
--- TestingMVVM/ViewModels/MainWindowViewModel.cs
+++ TestingMVVM/ViewModels/MainWindowViewModel.cs
@@ -3,20 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TestingMVVM.ViewModels.Base;
+using TestingMVVM.Infrastructure.Commands;
 
 namespace TestingMVVM.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Ввод страны
-        private string _CountryName;
+        #region Поле ввода названия страны
 
-        public string CountryName
+        private string _PartialUrl;
+
+        public string PartialUrl
         {
-            get => _CountryName;
-            set => _CountryName = value;
+            get => _PartialUrl;
+            set => _PartialUrl = value;
         }
+
         #endregion
+
+        #region Команды
+
+        #region SendHttpCommand
+
+        public ICommand SendHttpCommand { get; }
+
+        private bool CanSendHttpCommandExecute(object p) => true;
+
+        private void OnSendHttpCommandExecute(object p)
+        {
+
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+
+            #region Команды
+
+            SendHttpCommand = new LambdaCommand(OnSendHttpCommandExecute, CanSendHttpCommandExecute);
+
+            #endregion
+
+        }
+
     }
 }
